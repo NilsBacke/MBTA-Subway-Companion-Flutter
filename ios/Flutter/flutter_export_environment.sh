d41f5d4e17e39912ecc0a6c11cc679fc85b1348f
Nils Backe <nils.backe@gmail.com>
2019-08-21 19:57:21 -0400

    Work [ci skip] (#26)
    * solid maps work
    
    * solid maps work
    
    * fixing TODOs
    
    * boom boom bang
    
    * clearing errors
    
    * new icon
    
    * d
    
    * release 1.1.0
    
    * [ci skip] filtering and onboard button
    
    * bump version
    
    * 1.1.1
    
    * WORK 1.1.1

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/nilsbacke/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nilsbacke/Desktop/flutter/Boston-Subway-Companion-Flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/nilsbacke/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.1.1"
+export "FLUTTER_BUILD_NUMBER=7"
